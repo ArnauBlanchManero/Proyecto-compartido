98ebcc8664682be52a48b608d3a081d02ceaae95
AntonioAcoDurand <antonio.aco.dosa@gmail.com>
2024-10-30 11:28:47 +0100

    ejercicio1.1_Ingreso_de_Datos

--- Practica5/Prácticas/ejercicio1.c
+++ Practica5/Prácticas/ejercicio1.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+	int main () {
+	float tiempo[6] ;
+	printf("Introduzca la temperatura del lunes:\n");
+	scanf("%f",&tiempo[0]);
+	printf("Introduzca la temperatura del martes:\n");
+	scanf("%f",&tiempo[1]);
+	printf("Introduzca la temperatura del miercoles:\n");
+	scanf("%f",&tiempo[2]);
+	printf("Introduzca la temperatura del jueves:\n");
+	scanf("%f",&tiempo[3]);
+	printf("Introduzca la temperatura del viernes:\n");
+	scanf("%f",&tiempo[4]);
+	printf("Introduzca la temperatura del sabado:\n");
+	scanf("%f",&tiempo[5]);
+	printf("Introduzca la temperatura del domingo:\n");
+	scanf("%f",&tiempo[6]);
+	printf("%f",tiempo);
+	return EXIT_SUCCESS;
+	}
