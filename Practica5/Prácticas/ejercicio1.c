e4699c1f481d18ac0a279b82b278bd024b753ad4
ArnnauBlanchManero <arnau.b.manero@gmail.com>
2024-10-30 12:54:50 +0100

    Ingreso de datos en un bucle

--- Practica5/Prácticas/ejercicio1.c
+++ Practica5/Prácticas/ejercicio1.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float ingreso_datos(){
-
-	printf("Introduzca la temperatura del lunes:\n");
-	scanf("%f",&tiempo[0]);
+float ingreso_datos(float datos, int num, char dia[10]){
+		printf("Introduzca la temperatura del %s: ", dia);
+		scanf("%f",&datos);
+	
+	return datos;
 }
 
 float temperatura_promedio(){
@@ -12,8 +13,12 @@ float temperatura_promedio(){
 }
 
 int main () {
-	float tiempo[6] = ingreso_datos();
+	float tiempo[6];
+	char dia_semana[7][11]={"lunes", "martes", "miércoles", "jueves", "viernes", "sábado", "domingo"};
+	for (int i=0; i<7; i++){
+		tiempo[i] = ingreso_datos(tiempo[i], i, dia_semana[i]);
+	}
 	float promedio = temperatura_promedio();
 	return EXIT_SUCCESS;
-	}
+}
 	
