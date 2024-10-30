04c28c45f8c15f9fa40c96f4eec346c5daa22088
AntonioAcoDurand <antonio.aco.dosa@gmail.com>
2024-10-30 12:29:49 +0100

    DistribuciónDeFunciones

--- Practica5/Prácticas/ejercicio1.c
+++ Practica5/Prácticas/ejercicio1.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-	int main () {
-	float tiempo[6] ;
+
+float ingreso_datos(){
+
 	printf("Introduzca la temperatura del lunes:\n");
 	scanf("%f",&tiempo[0]);
-	printf("Introduzca la temperatura del martes:\n");
-	scanf("%f",&tiempo[1]);
-	printf("Introduzca la temperatura del miercoles:\n");
-	scanf("%f",&tiempo[2]);
-	printf("Introduzca la temperatura del jueves:\n");
-	scanf("%f",&tiempo[3]);
-	printf("Introduzca la temperatura del viernes:\n");
-	scanf("%f",&tiempo[4]);
-	printf("Introduzca la temperatura del sabado:\n");
-	scanf("%f",&tiempo[5]);
-	printf("Introduzca la temperatura del domingo:\n");
-	scanf("%f",&tiempo[6]);
-	printf("%f",tiempo);
+}
+
+float temperatura_promedio(){
+	
+}
+
+int main () {
+	float tiempo[6] = ingreso_datos();
+	float promedio = temperatura_promedio();
 	return EXIT_SUCCESS;
 	}
+	
