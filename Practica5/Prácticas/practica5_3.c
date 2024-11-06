25a319715be3c1938dfc18a662503306dfe26a2e
ArnnauBlanchManero <arnau.b.manero@gmail.com>
2024-11-06 11:22:42 +0100

    Empiezo el ejercicio 1

--- Practica5/Prácticas/practica5_3.c
+++ Practica5/Prácticas/practica5_3.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct articulo {
+	char nombre[21];
+	int cantidad;
+};
+
+int pedir_cantidad_productos(){
+	int cantidad;
+	printf("Cuántos productos quieres gestionar? (máximo de 10) ");
+	scanf("%d", &cantidad);
+	return cantidad;
+}
+
+
 int main(){
+// 1. Ingreso de productos y cantidades
+	int n_productos;
+	struct articulo producto;
+	do{
+		printf("Has introducido demasiados productos\n");
+		n_productos = pedir_cantidad_productos();
+	} while (n_productos > 10); // Compruebo que el usuario haya introducido menos de 10 productos
+	for (int i = 0; i < n_productos; i++){ // Se repite tantas veces como número de productos haya
+		printf("Nombre del producto %d: ", i);
+		scanf(" %s", producto.nombre);
+		printf("Cantidad de %s: ", producto.nombre);
+		scanf("%d", &producto.cantidad);
+	}
+// 2. Mostrar inventario completo
 	return 0;
 }
