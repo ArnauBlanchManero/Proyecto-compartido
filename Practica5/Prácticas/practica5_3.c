cfa4ef21f93423b2d3fa8f2768395a4845bc34ad
ArnnauBlanchManero <arnau.b.manero@gmail.com>
2024-11-06 11:59:00 +0100

    Mostrar inventario practica 3

--- Practica5/Prácticas/practica5_3.c
+++ Practica5/Prácticas/practica5_3.c
@@ -17,17 +17,21 @@ int pedir_cantidad_productos(){
 int main(){
 // 1. Ingreso de productos y cantidades
 	int n_productos;
-	struct articulo producto;
 	do{
 		printf("Has introducido demasiados productos\n");
 		n_productos = pedir_cantidad_productos();
 	} while (n_productos > 10); // Compruebo que el usuario haya introducido menos de 10 productos
+	struct articulo producto[n_productos];
 	for (int i = 0; i < n_productos; i++){ // Se repite tantas veces como número de productos haya
 		printf("Nombre del producto %d: ", i);
-		scanf(" %s", producto.nombre);
-		printf("Cantidad de %s: ", producto.nombre);
-		scanf("%d", &producto.cantidad);
+		scanf(" %s", producto[i].nombre);
+		printf("Cantidad de %s: ", producto[i].nombre);
+		scanf("%d", &producto[i].cantidad);
 	}
 // 2. Mostrar inventario completo
+	printf("Inventario Completo:\n");
+	for (int i = 0; i < n_productos; i++){ // Se repite tantas veces como número de productos haya
+		printf("Producto %d: %s - %d unidades\n", i, producto[i].nombre, producto[i].cantidad);
+	}
 	return 0;
 }
