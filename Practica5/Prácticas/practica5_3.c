5b6c81747919b549ef266e1e271205451a0273f4
ArnnauBlanchManero <arnau.b.manero@gmail.com>
2024-11-08 14:30:33 +0100

    Acabo la práctica 3?

--- Practica5/Prácticas/practica5_3.c
+++ Practica5/Prácticas/practica5_3.c
@@ -6,7 +6,7 @@ struct articulo {
 	int cantidad;
 };
 
-int pedir_cantidad_productos(){
+int pedir_cantidad_productos(){ // Creo una función para llamarla simpre que el usuario ingrese una cantidad mayor a 10
 	int cantidad;
 	printf("Cuántos productos quieres gestionar? (máximo de 10) ");
 	scanf("%d", &cantidad);
@@ -19,7 +19,6 @@ int main(){
 	int n_productos;
 
 	do{
-		printf("Has introducido demasiados productos\n");
 		n_productos = pedir_cantidad_productos();
 	} while (n_productos > 10); // Compruebo que el usuario haya introducido menos de 10 productos
 
@@ -42,18 +41,62 @@ int main(){
 // 3. Rebastecimiento de productos
 	
 	char respuesta[4];
-	char rebastecer_producto[100];
-	int i = 0;
-	printf("¿Desea rebastecer algún producto? (sí/no): ");
-	scanf(" %s", respuesta);
-	if (strcmp(respuesta, "sí")){
-		printf("Ingrese el nombre del producto: ");
-		scanf(" %s", rebastecer_producto);
-		while(strcmp(producto[i].nombre, rebastecer_producto)){
-			i++;
+	char reabastecer_producto[100];
+	int cant = 0;
+	int i;
+	do {
+		printf("¿Desea reabastecer algún producto? (sí/no): ");
+		scanf(" %s", respuesta); 
+		if (strcmp(respuesta, "sí") == 0 || strcmp(respuesta, "si") == 0){ // Puede reabastecer productos sólo si escribe "si" o "sí"
+			printf("Ingrese el nombre del producto: ");
+			scanf(" %s", reabastecer_producto);
+			i = 0;
+			for (i = 0; i < n_productos; i++){
+				if (strcmp(producto[i].nombre, reabastecer_producto) == 0){ // Para buscar el producto
+					printf("Cantidad a añadir: ");
+					scanf("%d", &cant);
+					producto[i].cantidad += cant; // Se incrementa
+					break;
+				}
+			}
 		}
-		printf("Cantidad a añadir: ");
-		scanf("%d", &producto[i].cantidad);
+	}while (strcmp(respuesta, "no") != 0); // Sólo sale del bucle si pone la palabra "no"
+
+// 4. Cálculo de productos en baja existencia 
+
+	printf("Productos en baja existencia:\n");
+	for (int i = 0; i < n_productos; i++){ // Se repite tantas veces como número de productos haya
+		if (producto[i].cantidad < 5)
+			printf("%s - %d unidades\n", producto[i].nombre, producto[i].cantidad);
 	}
+
+// 5. Busca un producto por nombre
+	
+	char busca[100];
+	int j;
+	printf("Ingrese el nombre del producto a buscar: ");
+	scanf(" %s", busca);
+	for (j = 0; j < n_productos; j++){
+		if (strcmp(producto[j].nombre, busca) == 0){ // Cuando encuentro el nombre, imprimo su stock y salgo del bucle
+			printf("Cantidad en stock: %d unidades\n", producto[j].cantidad);
+			break;
+		}else if (j ==  n_productos-1){ // Si no encuentra el nombre del producto (llega a n_productos-1) imprime que no existe
+			printf("El producto no existe\n");
+		}
+	}
+
+// 6. Meta de stock diario
+	
+	int meta;
+	printf("Cuál es tu meta de stock diario: ");
+	scanf("%d", &meta);
+	for (int i = 0; i < n_productos; i++){
+		if(producto[i].cantidad == meta){
+			printf("El primer día en el que se alcanzó la meta es %s\n", producto[i].nombre);
+		}else if(i == n_productos-1){
+			printf("Ningún producto ha alcanzado la meta");
+		}
+	}
+
 	return 0;
 }
